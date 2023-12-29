a416a562f1cd5028817520e78cebdf8f9cab51c7
Giuliano Furgol <giulianofurgol@hotmail.com>
2023-12-29 19:33:22 +0100

    Trying to fix deploy on Heroku v02

--- app.js
+++ app.js
@@ -1,15 +1,11 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-// app.listen(port, () => {
-//   console.log(`App listening at http://localhost:${port}`);
-// });
-
-app.listen(port, () => {
-  console.log(`App is running`);
+app.listen(PORT, () => {
+    console.log(`App is running on port ${PORT}`);
 });
